a047c63e57673137007a395b33551e75e308399e
dimayak <412813+dimayak@users.noreply.github.com>
2024-03-24 15:06:01 +0100

    Some tiding up

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -4,8 +4,9 @@ public class Head : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [SerializeField] private Transform _pointer;
-    [SerializeField] private Vector3 _minRotation, _maxRotation;
-    [SerializeField] private float _minAngle, _maxAngle;
+    [Header("Head rotation")]
+    [SerializeField] private float _minAngle;
+    [SerializeField] private float _maxAngle;
     [SerializeField] private float _rotationSpeed;
 
     void Update()
@@ -13,15 +14,11 @@ public class Head : MonoBehaviour
         transform.position = _target.position;
 
         // Поворачиваем голову
-        float targetAngle = 0.0f;
+        float targetAngle = _minAngle;
         // У Pointer два значения вращения по y - 90 и 270
         if (_pointer.rotation.eulerAngles.y == 270)
         {
             targetAngle = _maxAngle;
-        }
-        else
-        {
-            targetAngle = _minAngle;
         }
         float currentAngle = transform.rotation.eulerAngles.y;
         // Вращаем просто по Оси Y на угол от _minAngle до _maxAngle
